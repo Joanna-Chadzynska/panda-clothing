844ee5909e26451f8eca1e1046b79849c5ee2559
Joanna Chądzyńska <jmalek87@gmail.com>
2021-02-13 12:59:34 +0100

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { Home, NotFound, Shop } from 'pages';
+import { Header } from 'components';
+import { Home, NotFound, Shop, SignInSignUp } from 'pages';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/'>
 					<Home />
 				</Route>
 				<Route path='/shop'>
 					<Shop />
+				</Route>
+				<Route path='/signin'>
+					<SignInSignUp />
 				</Route>
 				{/* <Route path='/shop/:id'>
 					<HatsPage />

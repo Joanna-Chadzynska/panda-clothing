2b5dc647e0a33f6f9df3a91973e07d6482e72219
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-11 17:38:33 +0100

    updating store: add middleware for ability to working with non-serializables values

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,21 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import {
+	Action,
+	configureStore,
+	getDefaultMiddleware,
+	ThunkAction,
+} from '@reduxjs/toolkit';
 import { rootReducer } from 'redux/reducer';
 
 export const store = configureStore({
 	reducer: rootReducer,
+	middleware: getDefaultMiddleware({
+		serializableCheck: {
+			// Ignore these field paths in all actions
+			ignoredActionPaths: ['payload.user.createdAt'],
+			// Ignore these paths in the state
+			ignoredPaths: ['user.currentUser.user.createdAt'],
+		},
+	}),
 });
 
 export type RootState = ReturnType<typeof store.getState>;

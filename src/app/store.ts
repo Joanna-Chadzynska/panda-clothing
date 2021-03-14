75f3a0cfc77fd79bf8013062427029b037b1794d
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-14 17:51:56 +0100

    setup redux-persitst

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,19 +4,42 @@ import {
 	getDefaultMiddleware,
 	ThunkAction,
 } from '@reduxjs/toolkit';
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	persistReducer,
+	persistStore,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from 'redux/reducer';
 
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+	whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: getDefaultMiddleware({
 		serializableCheck: {
 			// Ignore these field paths in all actions
 			ignoredActionPaths: ['payload.user.createdAt'],
 			// Ignore these paths in the state
 			ignoredPaths: ['user.currentUser.user.createdAt'],
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
 		},
 	}),
 });
+
+export let persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import { store } from 'app/store';
+import { persistor, store } from 'app/store';
 import 'assets/styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<Router>
-				<App />
-			</Router>
+			<PersistGate loading={null} persistor={persistor}>
+				<Router>
+					<App />
+				</Router>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')

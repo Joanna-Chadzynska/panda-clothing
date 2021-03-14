1e08c42fd5f4d7168ab26296e763c3f27bdd4c6e
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-14 18:15:15 +0100

    create new reducers: directory, shop

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,12 +1,16 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import cartSlice from './cart/cartSlice';
 import counterSlice from './counter/counterSlice';
+import directorySlice from './directory/directorySlice';
+import shopSlice from './shop/shopSlice';
 import userSlice from './user/userSlice';
 
 export const rootReducer = combineReducers({
 	counter: counterSlice,
 	user: userSlice,
 	cart: cartSlice,
+	directory: directorySlice,
+	shop: shopSlice,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;

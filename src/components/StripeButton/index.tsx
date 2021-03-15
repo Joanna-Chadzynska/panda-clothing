c20708a1f58800f49d899297f32ca833f821ebf1
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-15 15:06:05 +0100

    fix stripe type

--- src/components/StripeButton/index.tsx
+++ src/components/StripeButton/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import './styles/stripeButton.scss';
 
@@ -8,11 +8,17 @@ export interface StripeButtonProps {
 
 const StripeButton: React.SFC<StripeButtonProps> = ({ price }) => {
 	const priceForStripe = price * 100;
-	const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
+	const [publishableKey, setPublishableKey] = useState<any>('');
+
 	const onToken = (token: any) => {
 		console.log(token);
 		alert('Payment Successful');
 	};
+
+	useEffect(() => {
+		setPublishableKey(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+		return () => {};
+	}, []);
 	return (
 		<StripeCheckout
 			label='Pay Now'

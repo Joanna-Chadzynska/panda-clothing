e99dfbca77e1dd29a7de34a72bc4335c07e4276e
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-16 19:23:42 +0100

    udpate styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,10 +3,15 @@ import { CartDropdown, CartIcon } from 'components';
 import { auth } from 'firebase/firebase.utils';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { selectCartHidden } from 'redux/cart/cartSlice';
 import { selectCurrentUser } from 'redux/user/userSlice';
-import './styles/header.scss';
+import {
+	HeaderContainer,
+	LogoContainer,
+	Option,
+	OptionButton,
+	OptionsContainer,
+} from './styles/header';
 
 export interface HeaderProps {}
 
@@ -14,30 +19,22 @@ const Header: React.SFC<HeaderProps> = () => {
 	const currentUser = useSelector(selectCurrentUser);
 	const cartHidden = useSelector(selectCartHidden);
 	return (
-		<div className='header wrapper'>
-			<Link className='logo-container' to='/'>
-				<Logo className='logo' />
-			</Link>
-			<div className='options'>
-				<Link className='option' to='/shop'>
-					SHOP
-				</Link>
-				<Link className='option' to='/shop'>
-					CONTACT
-				</Link>
+		<HeaderContainer className='wrapper'>
+			<LogoContainer to='/'>
+				<Logo />
+			</LogoContainer>
+			<OptionsContainer>
+				<Option to='/shop'>SHOP</Option>
+				<Option to='/shop'>CONTACT</Option>
 				{currentUser ? (
-					<div className='option' onClick={() => auth.signOut()}>
-						SIGN OUT
-					</div>
+					<OptionButton onClick={() => auth.signOut()}>SIGN OUT</OptionButton>
 				) : (
-					<Link className='option' to='/signin'>
-						SIGN IN
-					</Link>
+					<Option to='/signin'>SIGN IN</Option>
 				)}
 				<CartIcon />
-			</div>
+			</OptionsContainer>
 			{cartHidden ? null : <CartDropdown />}
-		</div>
+		</HeaderContainer>
 	);
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
 import { Directory } from 'components';
 import * as React from 'react';
-import './styles/homepage.scss';
+import { HomepageContainer } from './styles/homepage';
+
 export interface HomeProps {}
 
-const Home: React.SFC<HomeProps> = () => {
-	return (
-		<div className='homepage'>
-			<Directory />
-		</div>
-	);
-};
+const Home: React.SFC<HomeProps> = () => (
+	<HomepageContainer>
+		<Directory />
+	</HomepageContainer>
+);
 
 export default Home;

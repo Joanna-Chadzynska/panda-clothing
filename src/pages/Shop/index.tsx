8cd59900d78ef1bef5da589f140da879dd7b782c
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-17 20:25:57 +0100

    update shopPage

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,20 +1,43 @@
-import { CollectionsOverview } from 'components';
+import { CollectionsOverview, Spinner } from 'components';
+import {
+	convertCollectionsSnapshotToMap,
+	firestore,
+} from 'firebase/firebase.utils';
 import { Collection } from 'pages';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route } from 'react-router';
 import { useRouteMatch } from 'react-router-dom';
+import { updateCollections } from 'redux/shop/shopSlice';
+
 export interface ShopProps {}
 
 const Shop: React.SFC<ShopProps> = () => {
 	const routeMatch = useRouteMatch();
+	const dispatch = useDispatch();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const collectionRef = firestore.collection('collections');
+		const unsubscribeFromSnapshot = collectionRef.onSnapshot(
+			async (snapshot) => {
+				const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+				dispatch(updateCollections(collectionsMap));
+				setIsLoading(false);
+			}
+		);
+
+		return () => unsubscribeFromSnapshot();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<div className='shop-page wrapper'>
 			<Route exact path={`${routeMatch.path}`}>
-				<CollectionsOverview />
+				{isLoading ? <Spinner /> : <CollectionsOverview />}
 			</Route>
 			<Route exact path={`${routeMatch.path}/:collection`}>
-				<Collection />
+				{isLoading ? <Spinner /> : <Collection />}
 			</Route>
 		</div>
 	);

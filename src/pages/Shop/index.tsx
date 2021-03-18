daa7fb35ff15c26c9c06a30e658ede3e6db811fb
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-18 18:21:07 +0100

    update shop page

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,43 +1,33 @@
 import { CollectionsOverview, Spinner } from 'components';
-import {
-	convertCollectionsSnapshotToMap,
-	firestore,
-} from 'firebase/firebase.utils';
 import { Collection } from 'pages';
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route } from 'react-router';
 import { useRouteMatch } from 'react-router-dom';
 import { updateCollections } from 'redux/shop/shopSlice';
+import { selectCollectionsFetching } from './../../redux/shop/shopSlice';
 
 export interface ShopProps {}
 
 const Shop: React.SFC<ShopProps> = () => {
 	const routeMatch = useRouteMatch();
 	const dispatch = useDispatch();
-	const [isLoading, setIsLoading] = useState(true);
+	const loading = useSelector(selectCollectionsFetching);
 
 	useEffect(() => {
-		const collectionRef = firestore.collection('collections');
-		const unsubscribeFromSnapshot = collectionRef.onSnapshot(
-			async (snapshot) => {
-				const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-				dispatch(updateCollections(collectionsMap));
-				setIsLoading(false);
-			}
-		);
+		dispatch(updateCollections());
 
-		return () => unsubscribeFromSnapshot();
+		return () => {};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
 		<div className='shop-page wrapper'>
 			<Route exact path={`${routeMatch.path}`}>
-				{isLoading ? <Spinner /> : <CollectionsOverview />}
+				{loading ? <Spinner /> : <CollectionsOverview />}
 			</Route>
 			<Route exact path={`${routeMatch.path}/:collection`}>
-				{isLoading ? <Spinner /> : <Collection />}
+				{loading ? <Spinner /> : <Collection />}
 			</Route>
 		</div>
 	);

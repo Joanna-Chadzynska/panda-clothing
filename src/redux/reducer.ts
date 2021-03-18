aee27bce9c524668096fea7474559908216ee869
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-18 18:21:22 +0100

    remove counter slice import

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import cartSlice from './cart/cartSlice';
-import counterSlice from './counter/counterSlice';
 import directorySlice from './directory/directorySlice';
 import shopSlice from './shop/shopSlice';
 import userSlice from './user/userSlice';
 
 export const rootReducer = combineReducers({
-	counter: counterSlice,
 	user: userSlice,
 	cart: cartSlice,
 	directory: directorySlice,

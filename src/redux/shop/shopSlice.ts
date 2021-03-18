bd13ac6aa5086cc75ca5aa8c25a662e2a242f372
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-18 18:20:42 +0100

    create async action for fetching collections from firestore

--- src/redux/shop/shopSlice.ts
+++ src/redux/shop/shopSlice.ts
@@ -1,31 +1,51 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from '../../app/store';
+import {
+	convertCollectionsSnapshotToMap,
+	firestore,
+} from 'firebase/firebase.utils';
+import { AppDispatch, AppThunk, RootState } from '../../app/store';
 import { Collection } from './../../components/CollectionPreview/types';
 
 interface ShopState {
 	collections: {
 		[key: string]: Collection;
 	};
+	loading: boolean;
+	errorMessage: string | undefined;
 }
 
 const initialState: ShopState = {
 	collections: {},
+	loading: false,
+	errorMessage: '',
 };
 
 export const shopSlice = createSlice({
 	name: 'shop',
 	initialState,
 	reducers: {
-		updateCollections: (
+		updateCollectionsSuccess: (
 			state,
 			action: PayloadAction<{ [key: string]: Collection }>
 		) => {
+			state.loading = false;
 			state.collections = action.payload;
+		},
+		updateCollectionsStart: (state) => {
+			state.loading = true;
+		},
+		updateCollectionsFailure: (state, action) => {
+			state.loading = false;
+			state.errorMessage = action.payload;
 		},
 	},
 });
 
-export const { updateCollections } = shopSlice.actions;
+export const {
+	updateCollectionsSuccess,
+	updateCollectionsFailure,
+	updateCollectionsStart,
+} = shopSlice.actions;
 
 export const selectCollections = (state: RootState) => state.shop.collections;
 
@@ -34,11 +54,29 @@ export const selectCollection = (collectionUrlParam: any) => (
 ) =>
 	state.shop.collections ? state.shop.collections[collectionUrlParam] : null;
 
+export const selectCollectionsFetching = (state: RootState) =>
+	state.shop.loading;
+
 export const selectCollectionForPreview = (state: RootState) =>
 	state.shop.collections
 		? Object.keys(state.shop.collections).map(
 				(key) => state.shop.collections[key]
 		  )
 		: [];
+
+export const updateCollections = (): AppThunk => async (
+	dispatch: AppDispatch
+) => {
+	const collectionRef = firestore.collection('collections');
+	dispatch(updateCollectionsStart());
+
+	collectionRef
+		.get()
+		.then((snapshot) => {
+			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+			dispatch(updateCollectionsSuccess(collectionsMap));
+		})
+		.catch((error) => dispatch(updateCollectionsFailure(error.message)));
+};
 
 export default shopSlice.reducer;
